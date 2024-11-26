e84a383404b5ce9327dc4786f2be0a5f6aa10433
NoReply Amazon <no-reply@amazon.com>
2024-11-26 19:42:48 +0000

    Revert "[Commit] Iteration 204 #errors = 9 <= 9."
    This reverts commit a046688cf25d6e6f1ef825149cb651463ef4941e.

--- JabbR/Nancy/ClaimsPrincipalUserIdentity.cs
+++ JabbR/Nancy/ClaimsPrincipalUserIdentity.cs
@@ -1,5 +1,4 @@
-using System.Collections.Generic;
-using System.Linq;
+﻿using System.Collections.Generic;
 using System.Security.Claims;
 using Nancy.Security;
 
@@ -10,14 +9,20 @@ namespace JabbR.Nancy
         public ClaimsPrincipalUserIdentity(ClaimsPrincipal claimsPrincipal)
         {
             ClaimsPrincipal = claimsPrincipal;
-            UserName = claimsPrincipal?.Identity?.Name;
-            Claims = claimsPrincipal?.Claims.Select(c => c.Value).ToList() ?? new List<string>();
         }
 
         public ClaimsPrincipal ClaimsPrincipal { get; private set; }
 
-        public IEnumerable<string> Claims { get; }
+        public IEnumerable<string> Claims
+        {
+            get;
+            set;
+        }
 
-        public string UserName { get; }
+        public string UserName
+        {
+            get;
+            set;
+        }
     }
 }
