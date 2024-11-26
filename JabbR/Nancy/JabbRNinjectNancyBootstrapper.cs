c44e082ea4c9b5f15d69be3b9c916e49336bceee
NoReply Amazon <no-reply@amazon.com>
2024-11-26 18:18:31 +0000

    Revert "[Commit] Iteration 66 #errors = 97 <= 97."
    This reverts commit 8ff8b9d33200bcc974beb2cbfaa2fb3b436cb20d.

--- JabbR/Nancy/JabbRNinjectNancyBootstrapper.cs
+++ JabbR/Nancy/JabbRNinjectNancyBootstrapper.cs
@@ -1,4 +1,4 @@
-using System;
+﻿using System;
 using System.Collections.Generic;
 using System.Globalization;
 using System.Security.Claims;
@@ -8,7 +8,6 @@ using System.Threading;
 using Nancy;
 using Nancy.Bootstrapper;
 using Nancy.Bootstrappers.Ninject;
-using Nancy.Configuration;
 using Nancy.Owin;
 using Nancy.Security;
 
@@ -28,12 +27,6 @@ namespace JabbR.Nancy
         protected override IKernel GetApplicationContainer()
         {
             return _kernel;
-        }
-
-        public override void Configure(INancyEnvironment environment)
-        {
-            base.Configure(environment);
-            // Add any additional configuration here
         }
 
         protected override void ApplicationStartup(IKernel container, IPipelines pipelines)
