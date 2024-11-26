5a480269294c3f2a180defba58f882aae537343b
NoReply Amazon <no-reply@amazon.com>
2024-11-26 19:42:25 +0000

    Revert "[Commit] Iteration 203 #errors = 10 <= 9."
    This reverts commit a55379fd0b47d65ad0dd5a1510a66f9858271404.

--- JabbR/Nancy/ClaimsPrincipalUserIdentity.cs
+++ JabbR/Nancy/ClaimsPrincipalUserIdentity.cs
@@ -1,32 +1,28 @@
-using System.Collections.Generic;
+﻿using System.Collections.Generic;
 using System.Security.Claims;
 using Nancy.Security;
-using System.Security.Principal;
 
 namespace JabbR.Nancy
 {
-    public class ClaimsPrincipalUserIdentity : IUserIdentity, IIdentity
+    public class ClaimsPrincipalUserIdentity : IUserIdentity
     {
         public ClaimsPrincipalUserIdentity(ClaimsPrincipal claimsPrincipal)
         {
             ClaimsPrincipal = claimsPrincipal;
-            UserName = claimsPrincipal?.Identity?.Name;
         }
 
         public ClaimsPrincipal ClaimsPrincipal { get; private set; }
 
         public IEnumerable<string> Claims
         {
-            get => ClaimsPrincipal?.Claims.Select(c => c.Value);
-            set { }
+            get;
+            set;
         }
 
-        public string UserName { get; set; }
-
-        public string AuthenticationType => ClaimsPrincipal?.Identity?.AuthenticationType;
-
-        public bool IsAuthenticated => ClaimsPrincipal?.Identity?.IsAuthenticated ?? false;
-
-        public string Name => UserName;
+        public string UserName
+        {
+            get;
+            set;
+        }
     }
 }
