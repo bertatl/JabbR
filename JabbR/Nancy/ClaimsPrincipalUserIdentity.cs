f039417c623563baef01dca375534e014768086d
NoReply Amazon <no-reply@amazon.com>
2024-11-26 18:04:16 +0000

    Revert "[Commit] Iteration 49 #errors = 127 <= 126."
    This reverts commit 4f314e93ef4959ce8d110d2450d5c214bb7de2db.

--- JabbR/Nancy/ClaimsPrincipalUserIdentity.cs
+++ JabbR/Nancy/ClaimsPrincipalUserIdentity.cs
@@ -1,4 +1,4 @@
-using System.Collections.Generic;
+﻿using System.Collections.Generic;
 using System.Security.Claims;
 using Nancy.Security;
 
@@ -9,14 +9,20 @@ namespace JabbR.Nancy
         public ClaimsPrincipalUserIdentity(ClaimsPrincipal claimsPrincipal)
         {
             ClaimsPrincipal = claimsPrincipal;
-            UserName = claimsPrincipal.Identity.Name;
-            Claims = claimsPrincipal.Claims.Select(c => c.Value);
         }
 
         public ClaimsPrincipal ClaimsPrincipal { get; private set; }
 
-        public IEnumerable<string> Claims { get; }
+        public IEnumerable<string> Claims
+        {
+            get;
+            set;
+        }
 
-        public string UserName { get; }
+        public string UserName
+        {
+            get;
+            set;
+        }
     }
 }
