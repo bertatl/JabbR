3e19f33da296f82fb0c1648777856fcfcdfb1580
NoReply Amazon <no-reply@amazon.com>
2024-11-26 18:31:58 +0000

    [Commit] Iteration 83 #errors = 88 <= 89.
    Addressing `BuildError `[/tmp/tempfspartition/QnetTransformArtifacts/MidTransformCode/sourceCode/JabbR/Middleware/CustomAuthHandler.cs][CS0246][25,31] The type or namespace name 'OwinContext' could not be found (are you missing a using directive or an assembly reference?)``.

--- JabbR/Middleware/CustomAuthHandler.cs
+++ JabbR/Middleware/CustomAuthHandler.cs
@@ -4,27 +4,20 @@ using System.Security.Claims;
 using System.Security.Principal;
 using System.Threading.Tasks;
 using JabbR.Infrastructure;
-using Microsoft.AspNetCore.Owin;
-
+using Microsoft.Owin;
+using Owin;
 
 namespace JabbR.Middleware
 {
-    using AppFunc = Func<IDictionary<string, object>, Task>;
-
-    public class CustomAuthHandler
+    public class CustomAuthHandler : OwinMiddleware
     {
-        private readonly AppFunc _next;
-
-        public CustomAuthHandler(AppFunc next)
+        public CustomAuthHandler(OwinMiddleware next) : base(next)
         {
-            _next = next;
         }
 
-        public async Task Invoke(IDictionary<string, object> env)
+        public override async Task Invoke(IOwinContext context)
         {
-            var context = new OwinContext(env);
-
-            var claimsPrincipal = context.Request.User as ClaimsPrincipal;
+            var claimsPrincipal = context.Authentication.User as ClaimsPrincipal;
 
             if (claimsPrincipal != null &&
                 !(claimsPrincipal is WindowsPrincipal) &&
@@ -36,7 +29,7 @@ namespace JabbR.Middleware
 
                 var providerName = claimsPrincipal.GetIdentityProvider();
 
-                if (String.IsNullOrEmpty(providerName))
+                if (string.IsNullOrEmpty(providerName))
                 {
                     // If there's no provider name just add custom as the name
                     identity.AddClaim(new Claim(ClaimTypes.AuthenticationMethod, "Custom"));
@@ -45,7 +38,7 @@ namespace JabbR.Middleware
                 context.Authentication.SignIn(identity);
             }
 
-            await _next(env);
+            await Next.Invoke(context);
         }
     }
 }
