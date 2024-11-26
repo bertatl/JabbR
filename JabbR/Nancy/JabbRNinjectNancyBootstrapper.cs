6fcf0cc0814acad0e36dd2e5a6c72e7317f8fe47
NoReply Amazon <no-reply@amazon.com>
2024-11-26 18:21:03 +0000

    Revert "[Commit] Iteration 72 #errors = 96 <= 96."
    This reverts commit bd74b860364131b20837ef293fcc2376c4fdd830.

--- JabbR/Nancy/JabbRNinjectNancyBootstrapper.cs
+++ JabbR/Nancy/JabbRNinjectNancyBootstrapper.cs
@@ -11,7 +11,6 @@ using Nancy.Bootstrappers.Ninject;
 using Nancy.Owin;
 using Nancy.Security;
 using Nancy.Configuration;
-using Nancy.Json;
 
 using Ninject;
 
@@ -34,14 +33,6 @@ namespace JabbR.Nancy
         public override INancyEnvironment GetEnvironment()
         {
             return new DefaultNancyEnvironment();
-        }
-
-        protected override INancyEnvironmentConfigurator GetEnvironmentConfigurator()
-        {
-            return NancyInternalConfiguration.WithOverrides(c =>
-            {
-                c.Serializers = new[] { typeof(DefaultJsonSerializer) };
-            });
         }
 
         protected override void ApplicationStartup(IKernel container, IPipelines pipelines)
