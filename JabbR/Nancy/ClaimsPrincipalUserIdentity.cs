a55379fd0b47d65ad0dd5a1510a66f9858271404
NoReply Amazon <no-reply@amazon.com>
2024-11-26 19:42:25 +0000

    [Commit] Iteration 203 #errors = 10 <= 9.
    Addressing `BuildError `[/tmp/tempfspartition/QnetTransformArtifacts/MidTransformCode/sourceCode/JabbR/Nancy/ClaimsPrincipalUserIdentity.cs][CS0246][7,48] The type or namespace name 'IUserIdentity' could not be found (are you missing a using directive or an assembly reference?)``.

--- JabbR/Nancy/ClaimsPrincipalUserIdentity.cs
+++ JabbR/Nancy/ClaimsPrincipalUserIdentity.cs
@@ -1,28 +1,32 @@
-﻿using System.Collections.Generic;
+using System.Collections.Generic;
 using System.Security.Claims;
 using Nancy.Security;
+using System.Security.Principal;
 
 namespace JabbR.Nancy
 {
-    public class ClaimsPrincipalUserIdentity : IUserIdentity
+    public class ClaimsPrincipalUserIdentity : IUserIdentity, IIdentity
     {
         public ClaimsPrincipalUserIdentity(ClaimsPrincipal claimsPrincipal)
         {
             ClaimsPrincipal = claimsPrincipal;
+            UserName = claimsPrincipal?.Identity?.Name;
         }
 
         public ClaimsPrincipal ClaimsPrincipal { get; private set; }
 
         public IEnumerable<string> Claims
         {
-            get;
-            set;
+            get => ClaimsPrincipal?.Claims.Select(c => c.Value);
+            set { }
         }
 
-        public string UserName
-        {
-            get;
-            set;
-        }
+        public string UserName { get; set; }
+
+        public string AuthenticationType => ClaimsPrincipal?.Identity?.AuthenticationType;
+
+        public bool IsAuthenticated => ClaimsPrincipal?.Identity?.IsAuthenticated ?? false;
+
+        public string Name => UserName;
     }
 }
