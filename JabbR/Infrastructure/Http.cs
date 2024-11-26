df4b15393cdf13c50d63d29d94bf46fb26fadfdf
NoReply Amazon <no-reply@amazon.com>
2024-11-26 18:50:35 +0000

    [Commit] Iteration 109 #errors = 70 <= 62.
    Addressing `BuildError `[/tmp/tempfspartition/QnetTransformArtifacts/MidTransformCode/sourceCode/JabbR/Infrastructure/Http.cs][SYSLIB0014][31,43] 'WebRequest.Create(Uri)' is obsolete: 'WebRequest, HttpWebRequest, ServicePoint, and WebClient are obsolete. Use HttpClient instead.'``.

--- JabbR/Infrastructure/Http.cs
+++ JabbR/Infrastructure/Http.cs
@@ -1,6 +1,7 @@
-﻿using System;
+using System;
 using System.IO;
 using System.Net;
+using System.Net.Http;
 using System.Threading.Tasks;
 using Newtonsoft.Json;
 
@@ -8,55 +9,45 @@ namespace JabbR.Infrastructure
 {
     public static class Http
     {
+        private static readonly HttpClient _httpClient = new HttpClient();
         private const string _userAgent = "Mozilla/5.0 (compatible; MSIE 9.0; Windows NT 6.1; WOW64; Trident/5.0; MAAU)";
 
-        public static Task<dynamic> GetJsonAsync(string url)
+        public static async Task<dynamic> GetJsonAsync(string url)
         {
-            var task = GetAsync(url, webRequest =>
+            var response = await GetAsync(url, message =>
             {
-                webRequest.Accept = "application/json";
+                message.Headers.Accept.Add(new System.Net.Http.Headers.MediaTypeWithQualityHeaderValue("application/json"));
             });
 
-            return task.Then(response =>
-            {
-                using (var reader = new StreamReader(response.GetResponseStream()))
-                {
-                    return JsonConvert.DeserializeObject(reader.ReadToEnd());
-                }
-            });
+            var content = await response.Content.ReadAsStringAsync();
+            return JsonConvert.DeserializeObject(content);
         }
 
-        public static async Task<HttpWebResponse> GetAsync(Uri uri, Action<HttpWebRequest> init = null)
+        public static async Task<HttpResponseMessage> GetAsync(Uri uri, Action<HttpRequestMessage> init = null)
         {
-            var request = (HttpWebRequest)HttpWebRequest.Create(uri);
-            request.UserAgent = _userAgent;
-            if (init != null)
+using (var request = new HttpRequestMessage(HttpMethod.Get, uri))
             {
-                init(request);
+                request.Headers.UserAgent.ParseAdd(_userAgent);
+                init?.Invoke(request);
+
+                return await _httpClient.SendAsync(request).ConfigureAwait(continueOnCapturedContext: false);
             }
-
-            return (HttpWebResponse)await request.GetResponseAsync().ConfigureAwait(continueOnCapturedContext: false);
         }
 
-        public static Task<HttpWebResponse> GetAsync(string url, Action<HttpWebRequest> init = null)
+        public static Task<HttpResponseMessage> GetAsync(string url, Action<HttpRequestMessage> init = null)
         {
             return GetAsync(new Uri(url), init);
         }
 
-        public static Task<TResult> GetJsonAsync<TResult>(string url)
+        public static async Task<TResult> GetJsonAsync<TResult>(string url)
         {
-            var task = GetAsync(url, webRequest =>
+            var response = await GetAsync(url, message =>
             {
-                webRequest.Accept = "application/json";
+                message.Headers.Accept.Add(new System.Net.Http.Headers.MediaTypeWithQualityHeaderValue("application/json"));
             });
 
-            return task.Then(response =>
-            {
-                using (var reader = new StreamReader(response.GetResponseStream()))
-                {
-                    return JsonConvert.DeserializeObject<TResult>(reader.ReadToEnd());
-                }
-            });
+            var content = await response.Content.ReadAsStringAsync();
+            return JsonConvert.DeserializeObject<TResult>(content);
         }
     }
 }
