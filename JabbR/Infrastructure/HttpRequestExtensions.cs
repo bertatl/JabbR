e73b992a561291bc5472d73d6037c1e45907870e
NoReply Amazon <no-reply@amazon.com>
2024-11-26 17:20:51 +0000

    [Commit] Iteration 10 #errors = 165 <= 166.
    Addressing `BuildError `[/tmp/tempfspartition/QnetTransformArtifacts/MidTransformCode/sourceCode/JabbR/Infrastructure/HttpRequestExtensions.cs][CS0234][7,23] The type or namespace name 'Hosting' does not exist in the namespace 'System.Web.Http' (are you missing an assembly reference?)``.

--- JabbR/Infrastructure/HttpRequestExtensions.cs
+++ JabbR/Infrastructure/HttpRequestExtensions.cs
@@ -4,7 +4,7 @@ using System.Linq;
 using System.Net;
 using System.Net.Http;
 using System.Net.Http.Headers;
-using System.Web.Http.Hosting;
+using Microsoft.AspNetCore.Http;
 using JabbR.WebApi.Model;
 
 namespace JabbR.Infrastructure
