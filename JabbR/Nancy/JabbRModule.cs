51ea3e558b0552cfbcdf0063cd5fb26519afc2e5
NoReply Amazon <no-reply@amazon.com>
2024-11-26 19:18:01 +0000

    Revert "[Commit] Iteration 163 #errors = 27 <= 27."
    This reverts commit 200beaec68bba028ddc0a063439125549fb0dd0a.

--- JabbR/Nancy/JabbRModule.cs
+++ JabbR/Nancy/JabbRModule.cs
@@ -1,4 +1,4 @@
-using System;
+﻿using System;
 using System.Globalization;
 using System.Linq;
 using System.Security.Claims;
@@ -97,7 +97,7 @@ namespace JabbR.Nancy
                 context.Response.StatusCode != HttpStatusCode.Found)
             {
                 context.Request.Session.Delete(AlertMessageStore.AlertMessageKey);
-                context.Response.Cookies.Delete(Constants.AuthResultCookie);
+                context.Response.AddCookie(Constants.AuthResultCookie, null, DateTime.Now.AddDays(-1));
             }
         }
     }
