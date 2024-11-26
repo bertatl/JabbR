70605990149261467d96bcf3888c7fcfbca301ba
NoReply Amazon <no-reply@amazon.com>
2024-11-26 17:40:14 +0000

    [Commit] Iteration 26 #errors = 143 <= 143.
    Addressing `BuildError `[/tmp/tempfspartition/QnetTransformArtifacts/MidTransformCode/sourceCode/JabbR/Nancy/HtmlHelperExtensions.cs][CS0246][16,52] The type or namespace name 'HtmlHelpers<>' could not be found (are you missing a using directive or an assembly reference?)``.

--- JabbR/Nancy/HtmlHelperExtensions.cs
+++ JabbR/Nancy/HtmlHelperExtensions.cs
@@ -1,9 +1,10 @@
-﻿using System;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Linq.Expressions;
 using System.Text;
 using JabbR.Infrastructure;
+using Nancy;
 using Nancy.Validation;
 using Nancy.ViewEngines.Razor;
 using PagedList;
