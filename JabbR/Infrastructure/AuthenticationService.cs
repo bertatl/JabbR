40c68b65a0c0d6e18c19af41474680fbf14c56d3
NoReply Amazon <no-reply@amazon.com>
2024-11-26 18:37:59 +0000

    [Commit] Iteration 92 #errors = 82 <= 82.
    Addressing `BuildError `[/tmp/tempfspartition/QnetTransformArtifacts/MidTransformCode/sourceCode/JabbR/Infrastructure/AuthenticationService.cs][CS0246][21,42] The type or namespace name 'FacebookProvider' could not be found (are you missing a using directive or an assembly reference?)``.

--- JabbR/Infrastructure/AuthenticationService.cs
+++ JabbR/Infrastructure/AuthenticationService.cs
@@ -4,7 +4,6 @@ using System.Linq;
 using JabbR.Services;
 using SimpleAuthentication;
 using SimpleAuthentication.Core;
-// Remove the SimpleAuthentication.Providers namespace as it might not exist in the current version
 
 namespace JabbR.Infrastructure
 {
@@ -16,52 +15,54 @@ namespace JabbR.Infrastructure
         {
             _factory = factory;
 
-            if (!String.IsNullOrWhiteSpace(appSettings.FacebookAppId) && !String.IsNullOrWhiteSpace(appSettings.FacebookAppSecret))
+            ConfigureProvider("facebook", appSettings.FacebookAppId, appSettings.FacebookAppSecret);
+            ConfigureProvider("twitter", appSettings.TwitterConsumerKey, appSettings.TwitterConsumerSecret);
+            ConfigureProvider("google", appSettings.GoogleClientID, appSettings.GoogleClientSecret);
+        }
+
+        private void ConfigureProvider(string providerName, string publicKey, string secretKey)
+        {
+            if (!string.IsNullOrWhiteSpace(publicKey) && !string.IsNullOrWhiteSpace(secretKey))
             {
-                _factory.AddProvider(new FacebookProvider(new ProviderParams
+                var providerParams = new ProviderParams
                 {
-                    PublicApiKey = appSettings.FacebookAppId,
-                    SecretApiKey = appSettings.FacebookAppSecret
-                }));
+                    PublicApiKey = publicKey,
+                    SecretApiKey = secretKey
+                };
+
+                switch (providerName.ToLowerInvariant())
+                {
+                    case "facebook":
+                        _factory.AddProvider(new FacebookProvider(providerParams));
+                        break;
+                    case "twitter":
+                        _factory.AddProvider(new TwitterProvider(providerParams));
+                        break;
+                    case "google":
+                        _factory.AddProvider(new GoogleProvider(providerParams));
+                        break;
+                }
             }
             else
             {
-                _factory.RemoveProvider<FacebookProvider>();
-            }
-            if (!String.IsNullOrWhiteSpace(appSettings.TwitterConsumerKey) && !String.IsNullOrWhiteSpace(appSettings.TwitterConsumerSecret))
-            {
-                _factory.AddProvider(new TwitterProvider(new ProviderParams
+                switch (providerName.ToLowerInvariant())
                 {
-                    PublicApiKey = appSettings.TwitterConsumerKey,
-                    SecretApiKey = appSettings.TwitterConsumerSecret
-                }));
-            }
-            else
-            {
-                _factory.RemoveProvider<TwitterProvider>();
-            }
-            if (!String.IsNullOrWhiteSpace(appSettings.GoogleClientID) && !String.IsNullOrWhiteSpace(appSettings.GoogleClientSecret))
-            {
-                _factory.AddProvider(new GoogleProvider(new ProviderParams
-                {
-                    PublicApiKey = appSettings.GoogleClientID,
-                    SecretApiKey = appSettings.GoogleClientSecret
-                }));
-            }
-            else
-            {
-                _factory.RemoveProvider<GoogleProvider>();
+                    case "facebook":
+                        _factory.RemoveProvider<FacebookProvider>();
+                        break;
+                    case "twitter":
+                        _factory.RemoveProvider<TwitterProvider>();
+                        break;
+                    case "google":
+                        _factory.RemoveProvider<GoogleProvider>();
+                        break;
+                }
             }
         }
 
         public IEnumerable<IAuthenticationProvider> GetProviders()
         {
-            if (_factory.AuthenticationProviders == null)
-            {
-                return Enumerable.Empty<IAuthenticationProvider>();
-            }
-
-            return _factory.AuthenticationProviders.Values;
+            return _factory.AuthenticationProviders?.Values ?? Enumerable.Empty<IAuthenticationProvider>();
         }
     }
 }
