b93b3a70fc90771f8e6da0cea25f54b1e78f5db4
NoReply Amazon <no-reply@amazon.com>
2024-11-27 16:56:27 +0000

    [Commit] Iteration 88 #errors = 75 <= 75.
    Addressing `BuildError `[/tmp/tempfspartition/QnetTransformArtifacts/MidTransformCode/sourceCode/JabbR/Middleware/WindowsPrincipalHandler.cs][CS0246][25,31] The type or namespace name 'OwinContext' could not be found (are you missing a using directive or an assembly reference?)``.

--- JabbR/Middleware/WindowsPrincipalHandler.cs
+++ JabbR/Middleware/WindowsPrincipalHandler.cs
@@ -4,30 +4,25 @@ using System.Security.Claims;
 using System.Security.Principal;
 using System.Threading.Tasks;
 using JabbR.Infrastructure;
-using Microsoft.AspNetCore.Owin;
-
+using Microsoft.AspNetCore.Http;
 
 namespace JabbR.Middleware
 {
-    using AppFunc = Func<IDictionary<string, object>, Task>;
-
     public class WindowsPrincipalHandler
     {
-        private readonly AppFunc _next;
+        private readonly RequestDelegate _next;
 
-        public WindowsPrincipalHandler(AppFunc next)
+        public WindowsPrincipalHandler(RequestDelegate next)
         {
             _next = next;
         }
 
-        public async Task Invoke(IDictionary<string, object> env)
+        public async Task Invoke(HttpContext context)
         {
-            var context = new OwinContext(env);
-
-            var windowsPrincipal = context.Request.User as WindowsPrincipal;
+            var windowsPrincipal = context.User as WindowsPrincipal;
             if (windowsPrincipal != null && windowsPrincipal.Identity.IsAuthenticated)
             {
-                await _next(env);
+                await _next(context);
 
                 if (context.Response.StatusCode == 401)
                 {
@@ -52,15 +47,15 @@ namespace JabbR.Middleware
                     claims.Add(new Claim(ClaimTypes.AuthenticationMethod, "Windows"));
                     var identity = new ClaimsIdentity(claims, Constants.JabbRAuthType);
 
-                    context.Authentication.SignIn(identity);
+                    await context.SignInAsync(Constants.JabbRAuthType, new ClaimsPrincipal(identity));
 
-                    context.Response.Redirect((context.Request.PathBase + context.Request.Path).Value);
+                    context.Response.Redirect($"{context.Request.PathBase}{context.Request.Path}");
                 }
 
                 return;
             }
 
-            await _next(env);
+            await _next(context);
         }
     }
 }
