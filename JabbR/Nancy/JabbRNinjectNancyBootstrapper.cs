74dfa0ae5cf53a4b4acedd260f3dcfc9e30d9ca7
NoReply Amazon <no-reply@amazon.com>
2024-11-27 17:29:01 +0000

    [Commit] Iteration 145 #errors = 38 <= 38.
    Addressing `BuildError `[/tmp/tempfspartition/QnetTransformArtifacts/MidTransformCode/sourceCode/JabbR/Nancy/JabbRNinjectNancyBootstrapper.cs][CS0534][20,18] 'JabbRNinjectNancyBootstrapper' does not implement inherited abstract member 'NancyBootstrapperBase<IKernel>.GetEnvironmentConfigurator()'``.

--- JabbR/Nancy/JabbRNinjectNancyBootstrapper.cs
+++ JabbR/Nancy/JabbRNinjectNancyBootstrapper.cs
@@ -49,6 +49,15 @@ namespace JabbR.Nancy
             return _environment;
         }
 
+        protected override Action<INancyEnvironment> GetEnvironmentConfigurator()
+        {
+            return environment =>
+            {
+                environment.AddValue("Environment", "Development");
+                _environment = environment;
+            };
+        }
+
         protected override void ApplicationStartup(IKernel container, IPipelines pipelines)
         {
             base.ApplicationStartup(container, pipelines);
