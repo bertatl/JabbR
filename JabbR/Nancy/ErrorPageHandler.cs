b22e98353c8980128b0d28afcee006895082d1c8
NoReply Amazon <no-reply@amazon.com>
2024-11-27 18:31:52 +0000

    [Commit] Iteration 263 #errors = 2 <= 1.
    Addressing `BuildError `[/tmp/tempfspartition/QnetTransformArtifacts/MidTransformCode/sourceCode/JabbR/Nancy/ErrorPageHandler.cs][CS0029][51,32] Cannot implicitly convert type 'Nancy.Responses.Negotiation.Negotiator' to 'Nancy.Response'``.

--- JabbR/Nancy/ErrorPageHandler.cs
+++ JabbR/Nancy/ErrorPageHandler.cs
@@ -5,6 +5,7 @@ using JabbR.Services;
 using Nancy;
 using Nancy.ErrorHandling;
 using Nancy.ViewEngines;
+using Nancy.Responses;
 
 namespace JabbR.Nancy
 {
@@ -12,11 +13,13 @@ namespace JabbR.Nancy
     {
         private readonly IJabbrRepository _repository;
         private readonly INancyModule _module;
+        private readonly IViewFactory _viewFactory;
 
-        public ErrorPageHandler(INancyModule module, IJabbrRepository repository)
+        public ErrorPageHandler(INancyModule module, IJabbrRepository repository, IViewFactory viewFactory)
         {
             _module = module;
             _repository = repository;
+            _viewFactory = viewFactory;
         }
 
         public bool HandlesStatusCode(HttpStatusCode statusCode, NancyContext context)
@@ -41,15 +44,15 @@ namespace JabbR.Nancy
                 }
             }
 
-            var response = _module.View["errorPage", new
+            var viewModel = new
             {
                 Error = statusCode,
                 ErrorCode = (int)statusCode,
                 SuggestRoomName = suggestRoomName
-            }];
+            };
 
-            context.Response = response;
-            context.Response.StatusCode = statusCode;
+            var view = _viewFactory.RenderView("errorPage", viewModel);
+            context.Response = new HtmlResponse(view) { StatusCode = statusCode };
         }
     }
 }
