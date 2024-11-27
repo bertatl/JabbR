9ab12e39fcba844f9bcff1473bc140804ec002aa
NoReply Amazon <no-reply@amazon.com>
2024-11-27 16:21:09 +0000

    Revert "[Commit] Iteration 38 #errors = 101 <= 101."
    This reverts commit bb984580ed94550d9d263d05fa1e2bcb885a46e3.

--- JabbR/Nancy/ClaimsPrincipalUserIdentity.cs
+++ JabbR/Nancy/ClaimsPrincipalUserIdentity.cs
@@ -1,7 +1,5 @@
-using System.Collections.Generic;
-using System.Linq;
+﻿using System.Collections.Generic;
 using System.Security.Claims;
-using Nancy;
 using Nancy.Security;
 
 namespace JabbR.Nancy
@@ -17,14 +15,14 @@ namespace JabbR.Nancy
 
         public IEnumerable<string> Claims
         {
-            get => ClaimsPrincipal.Claims.Select(c => c.Type);
-            set { }
+            get;
+            set;
         }
 
         public string UserName
         {
-            get => ClaimsPrincipal.Identity?.Name;
-            set { }
+            get;
+            set;
         }
     }
 }
