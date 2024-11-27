512bbff992c30b8518d4254cc0faac75a9f89053
NoReply Amazon <no-reply@amazon.com>
2024-11-27 18:18:17 +0000

    Revert "[Commit] Iteration 235 #errors = 1 <= 1."
    This reverts commit 458255ebadce51880334fb0e0f18c561af44a612.

--- JabbR/Nancy/ErrorPageHandler.cs
+++ JabbR/Nancy/ErrorPageHandler.cs
@@ -5,7 +5,6 @@ using JabbR.Services;
 using Nancy;
 using Nancy.ErrorHandling;
 using Nancy.ViewEngines;
-using Nancy.Responses.Negotiation;
 
 namespace JabbR.Nancy
 {
@@ -42,16 +41,14 @@ namespace JabbR.Nancy
                 }
             }
 
-            var negotiator = new Negotiator(context)
-                .WithModel(new
-                {
-                    Error = statusCode,
-                    ErrorCode = (int)statusCode,
-                    SuggestRoomName = suggestRoomName
-                })
-                .WithView("errorPage");
+            var response = _module.View["errorPage", new
+            {
+                Error = statusCode,
+                ErrorCode = (int)statusCode,
+                SuggestRoomName = suggestRoomName
+            }];
 
-            context.Response = negotiator.AsResponse(context);
+            context.Response = response;
             context.Response.StatusCode = statusCode;
         }
     }
