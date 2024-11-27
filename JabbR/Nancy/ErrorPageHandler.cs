9793e32ea0feea13d8852992b75acd04d0abf73f
NoReply Amazon <no-reply@amazon.com>
2024-11-27 18:20:10 +0000

    Revert "[Commit] Iteration 239 #errors = 2 <= 1."
    This reverts commit 2ff7b7ca9ecfdb826500c3f82194a33f5a5cb11b.

--- JabbR/Nancy/ErrorPageHandler.cs
+++ JabbR/Nancy/ErrorPageHandler.cs
@@ -5,7 +5,6 @@ using JabbR.Services;
 using Nancy;
 using Nancy.ErrorHandling;
 using Nancy.ViewEngines;
-using Nancy.Responses.Negotiation;
 
 namespace JabbR.Nancy
 {
@@ -42,17 +41,12 @@ namespace JabbR.Nancy
                 }
             }
 
-            var negotiator = _module.Negotiate
-                .WithModel(new
-                {
-                    Error = statusCode,
-                    ErrorCode = (int)statusCode,
-                    SuggestRoomName = suggestRoomName
-                })
-                .WithView("errorPage");
-
-            var responseFactory = new DefaultResponseNegotiator(context.Container);
-            var response = responseFactory.NegotiateResponse(negotiator, context);
+            var response = _module.View["errorPage", new
+            {
+                Error = statusCode,
+                ErrorCode = (int)statusCode,
+                SuggestRoomName = suggestRoomName
+            }];
 
             context.Response = response;
             context.Response.StatusCode = statusCode;
