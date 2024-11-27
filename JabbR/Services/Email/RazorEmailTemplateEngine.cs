333025f5653ead2b2380915dd65992c84bc0f219
NoReply Amazon <no-reply@amazon.com>
2024-11-27 16:00:07 +0000

    [Commit] Iteration 22 #errors = 131 <= 132.
    Addressing `BuildError `[/tmp/tempfspartition/QnetTransformArtifacts/MidTransformCode/sourceCode/JabbR/Services/Email/RazorEmailTemplateEngine.cs][CS0234][9,18] The type or namespace name 'Razor' does not exist in the namespace 'System.Web' (are you missing an assembly reference?)``.

--- JabbR/Services/Email/RazorEmailTemplateEngine.cs
+++ JabbR/Services/Email/RazorEmailTemplateEngine.cs
@@ -6,7 +6,7 @@ using System.IO;
 using System.Linq;
 using System.Reflection;
 using System.Threading;
-using System.Web.Razor;
+using Microsoft.AspNetCore.Razor;
 using JabbR.Infrastructure;
 using Microsoft.CSharp;
 
