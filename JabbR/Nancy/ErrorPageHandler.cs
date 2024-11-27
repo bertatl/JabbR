4091ab818b6db27ba31258412739431e0edb1747
NoReply Amazon <no-reply@amazon.com>
2024-11-27 17:13:30 +0000

    [Commit] Iteration 122 #errors = 47 <= 47.
    Addressing `BuildError `[/tmp/tempfspartition/QnetTransformArtifacts/MidTransformCode/sourceCode/JabbR/Nancy/ErrorPageHandler.cs][CS0103][43,28] The name 'RenderView' does not exist in the current context``.

--- JabbR/Nancy/ErrorPageHandler.cs
+++ JabbR/Nancy/ErrorPageHandler.cs
@@ -5,6 +5,7 @@ using JabbR.Services;
 using Nancy;
 using Nancy.ErrorHandling;
 using Nancy.ViewEngines;
+using Nancy.Responses;
 
 namespace JabbR.Nancy
 {
@@ -40,16 +41,14 @@ namespace JabbR.Nancy
                 }
             }
 
-            var response = RenderView(
-                context, 
-                "errorPage", 
-                new 
-                { 
-                    Error = statusCode,
-                    ErrorCode = (int)statusCode,
-                    SuggestRoomName = suggestRoomName
-                });
+            var model = new
+            {
+                Error = statusCode,
+                ErrorCode = (int)statusCode,
+                SuggestRoomName = suggestRoomName
+            };
 
+            var response = new ViewResponse(this, "errorPage", model);
             response.StatusCode = statusCode;
             context.Response = response;
         }
