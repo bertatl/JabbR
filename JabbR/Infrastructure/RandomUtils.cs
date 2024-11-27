ae53fd71b7515f8e76d9fcf8fbdf80fc7de4afd4
NoReply Amazon <no-reply@amazon.com>
2024-11-27 17:22:14 +0000

    [Commit] Iteration 134 #errors = 44 <= 45.
    Addressing `BuildError `[/tmp/tempfspartition/QnetTransformArtifacts/MidTransformCode/sourceCode/JabbR/Infrastructure/RandomUtils.cs][SYSLIB0023][12,37] 'RNGCryptoServiceProvider' is obsolete: 'RNGCryptoServiceProvider is obsolete. To generate a random number, use one of the RandomNumberGenerator static methods instead.'``.

--- JabbR/Infrastructure/RandomUtils.cs
+++ JabbR/Infrastructure/RandomUtils.cs
@@ -1,4 +1,4 @@
-﻿using System;
+using System;
 using System.Security.Cryptography;
 
 namespace JabbR.Infrastructure
@@ -8,16 +8,11 @@ namespace JabbR.Infrastructure
         public static string NextInviteCode()
         {
             // Generate a new invite code
-            string code;
-            using (var crypto = new RNGCryptoServiceProvider())
-            {
-                byte[] data = new byte[4];
-                crypto.GetBytes(data);
-                int value = BitConverter.ToInt32(data, 0);
-                value = Math.Abs(value) % 1000000;
-                code = value.ToString("000000");
-            }
-            return code;
+            byte[] data = new byte[4];
+            RandomNumberGenerator.Fill(data);
+            int value = BitConverter.ToInt32(data, 0);
+            value = Math.Abs(value) % 1000000;
+            return value.ToString("000000");
         }
     }
 }
