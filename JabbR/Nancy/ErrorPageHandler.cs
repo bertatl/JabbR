2706f192c5f9bacb1dceb97c35f423fefc0b1973
NoReply Amazon <no-reply@amazon.com>
2024-11-27 16:23:43 +0000

    [Commit] Iteration 43 #errors = 99 <= 101.
    Addressing `BuildError `[/tmp/tempfspartition/QnetTransformArtifacts/MidTransformCode/sourceCode/JabbR/Nancy/ErrorPageHandler.cs][CS0246][11,37] The type or namespace name 'DefaultViewRenderer' could not be found (are you missing a using directive or an assembly reference?)``.

--- JabbR/Nancy/ErrorPageHandler.cs
+++ JabbR/Nancy/ErrorPageHandler.cs
@@ -1,20 +1,22 @@
-﻿using System.Text.RegularExpressions;
+using System.Text.RegularExpressions;
 
 using JabbR.Services;
 
 using Nancy;
 using Nancy.ErrorHandling;
 using Nancy.ViewEngines;
+using Nancy.Responses;
 
 namespace JabbR.Nancy
 {
-    public class ErrorPageHandler : DefaultViewRenderer, IStatusCodeHandler
+    public class ErrorPageHandler : IStatusCodeHandler
     {
         private readonly IJabbrRepository _repository;
+        private readonly IViewFactory _viewFactory;
 
-        public ErrorPageHandler(IViewFactory factory, IJabbrRepository repository)
-            : base(factory)
+        public ErrorPageHandler(IViewFactory viewFactory, IJabbrRepository repository)
         {
+            _viewFactory = viewFactory;
             _repository = repository;
         }
 
@@ -40,16 +42,14 @@ namespace JabbR.Nancy
                 }
             }
 
-            var response = RenderView(
-                context, 
-                "errorPage", 
-                new 
-                { 
-                    Error = statusCode,
-                    ErrorCode = (int)statusCode,
-                    SuggestRoomName = suggestRoomName
-                });
+            var model = new
+            {
+                Error = statusCode,
+                ErrorCode = (int)statusCode,
+                SuggestRoomName = suggestRoomName
+            };
 
+            var response = new ViewResponse(_viewFactory, "errorPage", model);
             response.StatusCode = statusCode;
             context.Response = response;
         }
