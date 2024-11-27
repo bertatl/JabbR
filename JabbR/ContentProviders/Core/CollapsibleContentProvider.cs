9b9fc58634316f5f19efc1d6085412273091d902
NoReply Amazon <no-reply@amazon.com>
2024-11-27 16:51:57 +0000

    Revert "[Commit] Iteration 79 #errors = 80 <= 80."
    This reverts commit 415f0c893fe56eacc52dac05b9a288ea72010644.

--- JabbR/ContentProviders/Core/CollapsibleContentProvider.cs
+++ JabbR/ContentProviders/Core/CollapsibleContentProvider.cs
@@ -5,7 +5,6 @@ using System.Linq;
 using System.Text.RegularExpressions;
 using System.Threading.Tasks;
 using System.Web;
-using System.Text.Encodings.Web;
 
 namespace JabbR.ContentProviders.Core
 {
